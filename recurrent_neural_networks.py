91acbf8e50947c737ececca5e9cc991414bec5c6
kshravi86 <kshravi86@gmail.com>
2025-03-10 12:21:53 +0530

    addeD

--- recurrent_neural_networks.py
+++ recurrent_neural_networks.py
@@ -38,3 +38,51 @@ model = keras.Sequential([
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test))
+
+# Mathematical Explanation of RNN:
+# -----------------------------
+# For each time step t, RNN computes:
+# h[t] = tanh(W_hh * h[t-1] + W_xh * x[t] + b_h)  # hidden state
+# y[t] = W_hy * h[t] + b_y                         # output
+#
+# Where:
+# - h[t]: hidden state at time t
+# - x[t]: input at time t
+# - y[t]: output at time t
+# - W_hh: weight matrix for hidden-to-hidden connections
+# - W_xh: weight matrix for input-to-hidden connections
+# - W_hy: weight matrix for hidden-to-output connections
+# - b_h: hidden bias vector
+# - b_y: output bias vector
+# - tanh: hyperbolic tangent activation function
+#
+# Loss Function (Mean Squared Error):
+# MSE = (1/n) * Σ(y_true - y_pred)²
+#
+# Backpropagation Through Time (BPTT):
+# The gradient at each time step t is:
+# ∂E/∂W = Σ(∂E/∂y[t] * ∂y[t]/∂h[t] * ∂h[t]/∂W)
+# where E is the error/loss function
+
+# Print mathematical explanations
+print("RNN Mathematical Formulas:")
+print("-------------------------")
+print("For each time step t, RNN computes:")
+print("h[t] = tanh(W_hh * h[t-1] + W_xh * x[t] + b_h)  # hidden state")
+print("y[t] = W_hy * h[t] + b_y                         # output")
+print("\nWhere:")
+print("- h[t]: hidden state at time t")
+print("- x[t]: input at time t")
+print("- y[t]: output at time t")
+print("- W_hh: weight matrix for hidden-to-hidden connections")
+print("- W_xh: weight matrix for input-to-hidden connections")
+print("- W_hy: weight matrix for hidden-to-output connections")
+print("- b_h: hidden bias vector")
+print("- b_y: output bias vector")
+print("- tanh: hyperbolic tangent activation function")
+print("\nLoss Function (Mean Squared Error):")
+print("MSE = (1/n) * Σ(y_true - y_pred)²")
+print("\nBackpropagation Through Time (BPTT):")
+print("The gradient at each time step t is:")
+print("∂E/∂W = Σ(∂E/∂y[t] * ∂y[t]/∂h[t] * ∂h[t]/∂W)")
+print("where E is the error/loss function")
